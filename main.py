b22bff753401d82db2103a5688a1a07891bd2168
aFish <tako0903@icloud.com>
2018-08-06 21:01:02 +0800

    main

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+from task import timedTask
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+ 
+@app.route('/', methods=['GET'])
+
+def home():
+    f = open('url_list.txt')
+    lines = f.readlines()
+    max_line = len(lines)
+    ranline = random.randint(0,max_line)
+    
+    data = {
+        'url':lines[ranline][:-1]
+    }
+
+    return jsonify(data)
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
+    timedTask()
