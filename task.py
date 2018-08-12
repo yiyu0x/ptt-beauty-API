4e98ad45df6470864def711ca9e672420a22c62e
aFish <tako0903@icloud.com>
2018-08-12 19:39:26 +0800

    balance url_list size

--- task.py
+++ task.py
@@ -1,13 +1,24 @@
 from datetime import datetime 
 from get_image import getUrl
 import time 
+import os
+
+# 60*30 , every 30 minutes will update
+frequency = 1800
+url_list_size = 5000
 
 def timedTask(): 
-	while True: 
+	while True:
+		file = open('url_list.txt')
 		print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+		print('start to get image ...')
 		getUrl()
-		# 60*30 , every 30 minutes will update
-		time.sleep(1800)
+		lines = len(file.readlines())
+		print('done, url_list.txt have',lines,'lines.')
+		if lines > url_list_size :
+			del_lines = str(lines - url_list_size)
+			os.system('sed -i \'1,'+del_lines+'d\' url_list.txt')
+		time.sleep(frequency)
 
 
 
