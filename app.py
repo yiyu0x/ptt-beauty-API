f7c7a5059a0bed50ef4d4e147fd041f6037e15fd
aFish <tako0903@icloud.com>
2018-08-14 20:46:59 +0800

    fix cors

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ import random
 from task import timedTask
 from flask import Flask, jsonify
 
-
+##########support CORS###########
+def after_request(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    return response
+#################################
 app = Flask(__name__)
+app.after_request(after_request)
  
 @app.route('/', methods=['GET'])
 def home():
